695655e136279f19a0be31c2506b5bf78d54c32e
mitsusaburo <bschindlerm@gmail.com>
2023-10-25 18:05:58 -0300

    Finish all excercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,55 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const titles = books.map((book) => book.title);
+    console.log(titles);
+    return(titles);
+
+
 
 };
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+getTheTitles(books);
+
+
+/*
+arg is an array of objects
+from each object, get one property and put in a new array
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,82 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+]
 
+
+
+const findTheOldest = function(people) {
+
+    const sortedAges = people.sort(function(person1, person2) {
+        if (!person1.yearOfDeath) {
+            person1.yearOfDeath = new Date().getFullYear();
+        }
+        if (!person2.yearOfDeath) {
+            person2.yearOfDeath = new Date().getFullYear();
+        }
+        return (person1.yearOfDeath - person1.yearOfBirth) - (person2.yearOfDeath - person2.yearOfBirth)
+    });
+    
+    return sortedAges.reverse()[0];
 };
+
+findTheOldest(people);
+
+
+/*
+
+take first object,
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
